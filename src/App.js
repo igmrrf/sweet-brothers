1c71378295ae7966c81f433504ed526616927a8f
igmrrf <francis.igbiriki.fi@gmail.com>
2020-08-04 12:28:42 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import { Homepage } from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 function App() {
   return (
-    <Switch>
-      <Route exact={true} path={"/"} component={Homepage} />
-      <Route exact={true} path={"/shop"} component={ShopPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact={true} path={"/"} component={Homepage} />
+        <Route exact={true} path={"/shop"} component={ShopPage} />
+      </Switch>
+    </>
   );
 }
 
