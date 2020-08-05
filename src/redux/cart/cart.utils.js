0127faf2836ef6d64318dc435e1a90cb6318bffd
igmrrf <francis.igbiriki.fi@gmail.com>
2020-08-06 00:16:23 +0100

    Implemented removing items from cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,14 @@ export const addItemToCart = (cartItems, cardItemToAdd) => {
   }
   return [...cartItems, { ...cardItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartToRemove.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartToRemove.id);
+  }
+  return cartItems;
+};
