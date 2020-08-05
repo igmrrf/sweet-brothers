e2816cb2b076529c3692afab1ff68991b8e99891
igmrrf <francis.igbiriki.fi@gmail.com>
2020-08-05 22:59:27 +0100

    Adding reselect to handle maping to props

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,6 +4,9 @@ import { ReactComponent as Logo } from "../../assets/img/logo.svg";
 import "./header.styles.scss";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
@@ -34,9 +37,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const stateMappingToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden,
+const stateMappingToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
 });
 
 export default connect(stateMappingToProps)(Header);
