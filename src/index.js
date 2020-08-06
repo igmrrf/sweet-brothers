0fd70f25ea9ef384146e1f93154f2d1c53b05020
igmrrf <francis.igbiriki.fi@gmail.com>
2020-08-06 01:41:18 +0100

    Adding redux-persist to handle localStorage and cart history

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+
 import App from "./App";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+// const middlewares = [logger];
+const store = createStore(rootReducer, applyMiddleware(logger));
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+const persistor = persistStore(store);
+export { store, persistor };
