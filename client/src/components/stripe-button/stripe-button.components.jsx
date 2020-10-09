7e1d570f86bfc59bc415ec4e0c9c1cc907bcb98f
igmrrf <francis.igbiriki.fi@gmail.com>
2020-10-09 03:46:52 +0100

    Stripe Backend

--- client/src/components/stripe-button/stripe-button.components.jsx
+++ client/src/components/stripe-button/stripe-button.components.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   console.log(price);
@@ -7,8 +8,19 @@ const StripeCheckoutButton = ({ price }) => {
   const publishableKey =
     "pk_test_51HDmCdDXFdSLUUcdOlxCDX6xW3LKz77WtornGCRsUvr7MrqZnZgx0LRqScE8pdBYZ73c6UWOpVhAFOpGjvw3OOLG00HZ23pqmt";
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment sent to the backend");
+    axios({
+      url:'payment',
+      method:'post',
+      data:{ 
+        amount:priceForStripe,
+      token,
+    }
+    }).then(response=>{
+      alert("Payment Successfull")
+    }).catch(error =>{
+      console.log("Payment error:", JSON.parse(error))
+      alert("There was an issue with your payment. Please ensue you use the provided cedit card")
+    })
   };
   return (
     <StripeCheckout
